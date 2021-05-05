6c869611756d8d618ec9536fe736b9a14c55c154
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-05-05 17:57:20 -0400

    Nested Scope Functions

--- day1.js
+++ day1.js
@@ -4,8 +4,8 @@
 // ##
 // ###
 // ####
-for (let line = "#"; line.length < 8; line += "#")
-  console.log(line);
+// for (let line = "#"; line.length < 8; line += "#")
+//   console.log(line);
 
 // challenge was to FizzBuzz depending on the conditions given
 // 1
@@ -13,33 +13,53 @@ for (let line = "#"; line.length < 8; line += "#")
 // Fizz
 // 4
 // Buzz
-for (let n = 1; n <= 100; n++) {
-  let output = "";
-  if (n % 3 == 0 && n % 5 == 0) output += "FizzBuzz";
-  else if (n % 3 == 0) output += "Fizz";
-  else if (n % 5 == 0) output += "Buzz";
+// for (let n = 1; n <= 100; n++) {
+//   let output = "";
+//   if (n % 3 == 0 && n % 5 == 0) output += "FizzBuzz";
+//   else if (n % 3 == 0) output += "Fizz";
+//   else if (n % 5 == 0) output += "Buzz";
   
-  console.log(output || n);
-}
+//   console.log(output || n);
+// }
 
 // challange was to creat a trinagle using hashtags:
 //  # # # #
 // # # # #
 //  # # # #
 // # # # #
-let size = 8;
+// let size = 8;
 
-let sqr = "";
+// let sqr = "";
 
-for (let y = 0; y < size; y++) {
-  for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) {
-      sqr += " ";
-    } else {
-      sqr += "#";
+// for (let y = 0; y < size; y++) {
+//   for (let x = 0; x < size; x++) {
+//     if ((x + y) % 2 == 0) {
+//       sqr += " ";
+//     } else {
+//       sqr += "#";
+//     }
+//   }
+//   sqr += "\n";
+// }
+
+// console.log(sqr);
+
+// found and interesting function for understanding Nested Scope
+
+const hummas = function(factor) {
+    const ingredient = function(amount, unit, name) {
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1) {
+            unit += "s";
+        }
+        console.log(`${ingredientAmount} ${unit} ${name}`);
     }
-  }
-  sqr += "\n";
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini");
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin");
 }
 
-console.log(sqr);
+hummas(14)
