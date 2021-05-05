857753ca9df3d479cfbbf0d68ba43f0d36315d7c
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-05-05 16:08:36 -0400

    second  challenge

--- day1.js
+++ day1.js
@@ -1,8 +1,23 @@
 
-// first challange was to creat a trinagle using hashtags:
+// challange was to creat a trinagle using hashtags:
 // #
 // ##
 // ###
 // ####
 for (let line = "#"; line.length < 8; line += "#")
   console.log(line);
+
+// challenge was to FizzBuzz depending on the conditions given
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0 && n % 5 == 0) output += "FizzBuzz";
+  else if (n % 3 == 0) output += "Fizz";
+  else if (n % 5 == 0) output += "Buzz";
+  
+  console.log(output || n);
+}
